656afcec9502ecf9755aa5ca0c90b7f94e231c54
Maxime Tessier <Maxime@MacBook-Pro-de-Maxime.local>
2017-04-21 16:48:06 -0400

    fix + nitroPickups + numeralSpeed

---
+++ SIForSpeedUnderground/Assets/Scripts/UIFillBasedOnVelocity.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFillBasedOnVelocity : MonoBehaviour {
+
+	public Rigidbody objectToMeasure;
+	public float minVelocity = 0.0f;
+	public float maxVelocity = 10.0f; 
+	private Image image;
+	public bool disallowBackwards = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		image = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(!disallowBackwards || Vector3.Dot(objectToMeasure.velocity, objectToMeasure.transform.forward) > 0.0f)
+		{
+			image.fillAmount = objectToMeasure.velocity.magnitude/maxVelocity;	
+		}
+
+		else
+		{
+			image.fillAmount = 0.0f;
+		}
+	}
+}
---
+++ SIForSpeedUnderground/Assets/Scripts/speedTextScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class speedTextScript : MonoBehaviour {
+
+	public Rigidbody objectToMeasure;
+	private Text text;
+	public bool disallowBackwards = false;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(!disallowBackwards || Vector3.Dot(objectToMeasure.velocity, objectToMeasure.transform.forward) > 0.0f)
+		{
+			float speed = objectToMeasure.velocity.magnitude;
+			speed *= 3.6f;
+
+			text.text = Mathf.Round(speed).ToString() + " KMH";
+
+			//text.text = objectToMeasure.velocity.magnitude.ToString();
+		}
+
+		else
+		{
+			text.text = "0 KMH";
+		}
+		
+	}
+}
